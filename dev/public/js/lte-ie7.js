a5375351474ca9ff22a28800a7e22f585d71812a
Matt Styles <matt@veryfizzyjelly.com>
2013-05-29 22:03:27 +0100

    Added icon font to theme for footer

---
+++ dev/public/js/lte-ie7.js
@@ -0,0 +1,31 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'vfj-icons\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-twitter' : '&#xe001;',
+			'icon-github' : '&#xe002;',
+			'icon-mail' : '&#xe003;',
+			'icon-open-source' : '&#xe000;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
